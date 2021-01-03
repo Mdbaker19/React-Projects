1ce8f7f74c2c86edd0ef87065c117d277a084627
Matthew <matthew.d.baker20@gmail.com>
2021-01-03 16:38:00 -0600

    added greeting with name

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+// a lot of Css can be split into separate css files and imported
 
+// functional components hooks
+
+import Matthew from "./matthew";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Matthew/>
       </header>
     </div>
   );
---
+++ src/matthew.js
@@ -0,0 +1,4 @@
+function Matthew(){
+    return <h3 className="nameHeader">Hello, my name is Matthew</h3>;
+}
+export default Matthew;
